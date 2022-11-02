356ba0c74073c24843caef5e0bf5213b56f954e6
myrzh <ishtarev@outlook.com>
2022-11-02 23:25:55 +0500

    create table

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
+import csv
 
-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QPushButton, QTableWidgetItem
 from PyQt5.QtGui import QFont
 from PyQt5 import QtCore, uic
 
@@ -9,11 +9,26 @@ from PyQt5 import QtCore, uic
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main_window.ui', self)  # Загружаем дизайн
         self.initUI()
-    
+        
     def initUI(self):
-        pass
+        uic.loadUi('main_window.ui', self)
+        self.loadTable('default_table.csv')
+
+    def loadTable(self, table_name):
+        with open(table_name, encoding="utf8") as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            title = next(reader)
+            self.tableWidget.setColumnCount(len(title))
+            self.tableWidget.setHorizontalHeaderLabels(title)
+            self.tableWidget.setRowCount(0)
+            for i, row in enumerate(reader):
+                self.tableWidget.setRowCount(
+                    self.tableWidget.rowCount() + 1)
+                for j, elem in enumerate(row):
+                    self.tableWidget.setItem(
+                        i, j, QTableWidgetItem(elem))
+        self.tableWidget.resizeColumnsToContents()
 
 
 if __name__ == '__main__':
