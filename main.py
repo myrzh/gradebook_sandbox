ed516dd75fb0ea1862d95722cb23cac58de96779
myrzh <ishtarev@outlook.com>
2022-11-03 21:40:03 +0500

    create add/delete subjects dialogs, def load_table

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import csv
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QPushButton, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QPushButton, QTableWidgetItem, QDialog, QFileDialog
 from PyQt5.QtGui import QFont
 from PyQt5 import QtCore, uic
 
@@ -13,9 +13,12 @@ class MainWindow(QMainWindow):
         
     def initUI(self):
         uic.loadUi('main_window.ui', self)
-        self.loadTable('default_table.csv')
+        self.show_table('default_table.csv')
+        self.add_subjects_button.clicked.connect(self.add_subjects_button_clicked)
+        self.delete_subjects_button.clicked.connect(self.delete_subjects_button_clicked)
+        self.load_table_button.clicked.connect(self.load_table)
 
-    def loadTable(self, table_name):
+    def show_table(self, table_name):
         with open(table_name, encoding="utf8") as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='"')
             title = next(reader)
@@ -29,6 +32,32 @@ class MainWindow(QMainWindow):
                     self.tableWidget.setItem(
                         i, j, QTableWidgetItem(elem))
         self.tableWidget.resizeColumnsToContents()
+    
+    def load_table(self):
+        fname = QFileDialog.getOpenFileName(self, 'Выбрать таблицу', '',
+                                            'Таблица CSV (*.csv);;Все файлы (*)')[0]
+
+    def add_subjects_button_clicked(self):
+        dlg = AddSubject()
+        dlg.exec()
+    
+    def delete_subjects_button_clicked(self):
+        dlg = DeleteSubject()
+        dlg.exec()
+
+
+class AddSubject(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('edit_subjects.ui', self)
+        self.setWindowTitle('Добавить предметы')
+        
+
+class DeleteSubject(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('edit_subjects.ui', self)
+        self.setWindowTitle('Удалить предметы')
 
 
 if __name__ == '__main__':
