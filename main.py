7ac90dcea191f9144a00897d2ae0cda0ecd8625b
myrzh <ishtarev@outlook.com>
2022-11-07 20:47:22 +0500

    create db + start accessing db

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
 import sys
 import csv
+import sqlite3
 
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QPushButton, QTableWidgetItem, QDialog, QFileDialog
-from PyQt5.QtGui import QFont
-from PyQt5 import QtCore, uic
+from PyQt5 import QtCore, uic, QtGui
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-        
+
     def initUI(self):
         uic.loadUi('main_window.ui', self)
+        self.setWindowTitle('JournalSandbox')
         self.show_table('default_table.csv')
+        self.subjects_db = sqlite3.connect("subjects.sqlite")
+
+        self.load_table_button.clicked.connect(self.load_table)
+        self.save_table_button.clicked.connect(self.save_table)
         self.add_subjects_button.clicked.connect(self.add_subjects_button_clicked)
         self.delete_subjects_button.clicked.connect(self.delete_subjects_button_clicked)
-        self.load_table_button.clicked.connect(self.load_table)
 
     def show_table(self, table_name):
         with open(table_name, encoding="utf8") as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='"')
             title = next(reader)
-            self.tableWidget.setColumnCount(len(title))
-            self.tableWidget.setHorizontalHeaderLabels(title)
-            self.tableWidget.setRowCount(0)
+            self.main_table.setColumnCount(len(title))
+            self.main_table.setHorizontalHeaderLabels(title)
+            self.main_table.setRowCount(0)
             for i, row in enumerate(reader):
-                self.tableWidget.setRowCount(
-                    self.tableWidget.rowCount() + 1)
+                self.main_table.setRowCount(
+                    self.main_table.rowCount() + 1)
                 for j, elem in enumerate(row):
-                    self.tableWidget.setItem(
+                    self.main_table.setItem(
                         i, j, QTableWidgetItem(elem))
-        self.tableWidget.resizeColumnsToContents()
-    
+        self.main_table.resizeColumnsToContents()
+
     def load_table(self):
-        fname = QFileDialog.getOpenFileName(self, 'Выбрать таблицу', '',
-                                            'Таблица CSV (*.csv);;Все файлы (*)')[0]
+        path = QFileDialog.getOpenFileName(self, 'Загрузить таблицу', '',
+                                           'Таблица CSV (*.csv)')[0]
+        if path:
+            self.show_table(path)
+    
+    def save_table(self):
+        path = QFileDialog.getSaveFileName(self, 'Сохранить таблицу', '',
+                                           'Таблица CSV (*.csv)')[0]
+        if path:
+            with open(str(path), 'wb') as stream:
+                writer = csv.writer(stream)
+                for row in range(self.main_table.rowCount()):
+                    rowdata = []
+                    for column in range(self.main_table.columnCount()):
+                        item = self.main_table.item(row, column)
+                        if item is not None:
+                            rowdata.append(
+                                str(item.text()).encode('utf8'))
+                        else:
+                            rowdata.append('')
+                    writer.writerow(rowdata)
+
 
     def add_subjects_button_clicked(self):
         dlg = AddSubject()
@@ -49,13 +73,19 @@ class MainWindow(QMainWindow):
 class AddSubject(QDialog):
     def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
         uic.loadUi('edit_subjects.ui', self)
         self.setWindowTitle('Добавить предметы')
-        
+ 
 
 class DeleteSubject(QDialog):
     def __init__(self):
         super().__init__()
+        self.initUI()
+    
+    def initUI(self):
         uic.loadUi('edit_subjects.ui', self)
         self.setWindowTitle('Удалить предметы')
 
